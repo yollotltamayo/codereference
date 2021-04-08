0a75ebc3c5414bda7d5b7334250c5b165e8f4d9b
Bruno A. Muciño <mucinoab@gmail.com>
2021-04-07 23:15:22 -0500

    Limpieza general backend
    - Actualiza dependencias
    - Quita dependencias y features sin usar
    - Mejor manejo de errores
    - Cargo clippy
    - Cargo fmt
    - Nueva variable de entorno "MONGO_DB"

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,61 +1,66 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-use serde::{Serialize, Deserialize};
-#[macro_use] extern crate rocket;
-use rocket_contrib::json::Json;
-use mongodb::{bson::doc, options::ClientOptions, sync::Client};
-pub mod content {
-    use mongodb::{bson::doc, options::ClientOptions,sync::Client};
-    use serde::{Serialize, Deserialize};
-    #[derive(Serialize,Deserialize)]
-    pub struct Codigo {
-        pub author: String, 
-        pub content: String,
-    }
-    pub struct Bro {
-        pub client: Client
-    }
-    impl Bro {
-        //#[tokio::main]
-        //pub async fn connect(&mut self)->mongodb::error::Result<()>{
-        pub fn connect()->Self{
-            let mut client_options =SECRETS;
+#![feature(decl_macro)]
 
-            Self{
-                  client: Client::with_uri_str(client_options).unwrap()
-            }
-            //let collection = db.collection("contenido")
-            //Ok(())
+use mongodb::{bson::doc, sync::Client};
+use rocket_contrib::json::{Json, JsonValue};
+use serde::{Deserialize, Serialize};
+
+#[macro_use]
+extern crate rocket;
+
+#[macro_use]
+extern crate rocket_contrib;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Codigo {
+    pub author: String,
+    pub content: String,
+}
+
+pub struct Bro {
+    pub client: Client,
+}
+
+impl Bro {
+    pub fn connect() -> Self {
+        let client_options = env!("MONGO_DB");
+
+        Self {
+            client: Client::with_uri_str(client_options).unwrap(),
         }
-        pub fn inserta(&self, data:Vec<mongodb::bson::Document>) ->mongodb::error::Result<()> {
-            let collection = self.client.database("Reference").collection("contenido");
-            collection.insert_many(data,None)?;
-            Ok(())
+    }
+
+    pub fn inserta(&self, data: Vec<mongodb::bson::Document>) {
+        let collection = self.client.database("Reference").collection("contenido");
+
+        if let Err(e) = collection.insert_many(data, None) {
+            eprintln!("Error al insertar: {}", e);
         }
     }
 }
-#[post("/submit",data="<codigo>")]
-    fn submit(codigo:Json<content::Codigo>) {
-        let conexion = content::Bro::connect();
-        let docs = vec![
-            doc!{"sku":"12", "autor":"yollotl" ,"codigo":&codigo.content}
-        ];
-        conexion.inserta(docs);
-    }
+
+#[post("/submit", data = "<codigo>")]
+fn submit(codigo: Json<Codigo>) {
+    let conexion = Bro::connect();
+    let docs = vec![doc! {"sku":"12", "autor":"yollotl" ,"codigo": &codigo.content}];
+    conexion.inserta(docs);
+}
+
 #[get("/")]
-    fn code() -> Json<Vec<content::Codigo>>{
-        Json(
-            vec![
-            content::Codigo{
-                author : "yo mero".to_string(), 
-                content: " fn main()  {) }".to_string()
-            },
-            content::Codigo{
-                author : "yo mero".to_string(), 
-                content: " fn main()  {) }".to_string()
-            }
-            ]
-        )
-    }
-    fn main()  {
-        rocket::ignite().mount("/api", routes![code,submit]).launch();
-    }
+fn code() -> JsonValue {
+    json!(vec![
+        Codigo {
+            author: "yo mero".to_string(),
+            content: " fn main()  {) }".to_string(),
+        },
+        Codigo {
+            author: "yo mero".to_string(),
+            content: " fn main()  {) }".to_string(),
+        },
+    ])
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/api", routes![code, submit])
+        .launch();
+}
