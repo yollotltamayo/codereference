aa8373ac5433f435db8d822f57ee0516bab7bc56
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2021-04-16 16:48:41 -0500

    udpate .gitignore

--- backend/env.sh
+++
@@ -1,4 +0,0 @@
-export CLIENT_SECRET="2f8a660c689f82dcc0bf7673e3d4a8f80b1ae715"
-export MONGO_URI="mongodb+srv://yollotl:%23Searsmeentiende2@cluster-1.qo9uf.mongodb.net/Reference?retryWrites=true&w=majority"
-export CLIENT_ID="ca3cac765529f9d577ec"
-export REDIR_URI="http://localhost:8000/login/github/callback"
